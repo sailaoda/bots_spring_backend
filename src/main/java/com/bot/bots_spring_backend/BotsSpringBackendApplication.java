f7ba51dc3bd9306e3d5109c9f6d4fbe915c3277b
sailaoda <1520516332@qq.com>
2022-11-08 02:11:08 +0800

    【特性】：初始化

---
+++ src/main/java/com/bot/bots_spring_backend/BotsSpringBackendApplication.java
@@ -0,0 +1,13 @@
+package com.bot.bots_spring_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BotsSpringBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BotsSpringBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/bot/bots_spring_backend/BotsSpringBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bot.bots_spring_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BotsSpringBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
